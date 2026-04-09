8185bdf7569847407f923d7992009d29ce931eb2
sa1and <64575580+sa1and@users.noreply.github.com>
2026-04-10 04:56:28 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+EmotionDetection package.
+
+Provides the emotion_detector function for analysing text emotions
+using the Watson NLP Embedded Library API.
+"""
+
+from .emotion_detection import emotion_detector
